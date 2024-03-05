a8d12add57eb66b6d8e409c8485fd88e72f44bd0
Nicholas-Battista <battistan23@gmail.com>
2024-03-05 16:12:27 -0700

    created handleCellClick function
    adds an eventlistner for each cell created, checks
    the gameboard for an instance of ship or null, and
    displays correctly on screen

--- src/DOM.js
+++ src/DOM.js
@@ -10,14 +10,19 @@ const displayPlayer = () => {
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       const cell = document.createElement("div");
+
       cell.classList.add("computerCell");
+
       if (player.board.board[i][j] instanceof Ship) {
         cell.style.backgroundColor = "lightgreen";
       }
+
       playerBoard.appendChild(cell);
     }
   }
 };
+
+// object with two value - dom and refernce - dom creates div - reference stores value inside array
 
 const displayComputer = () => {
   const computerBoard = document.querySelector(".computerBoard");
@@ -27,11 +32,22 @@ const displayComputer = () => {
       const cell = document.createElement("div");
       cell.classList.add("computerCell");
       computerBoard.appendChild(cell);
+      cell.addEventListener("click", () =>
+        handleCellClick(i, j, computer, cell)
+      );
     }
   }
 };
 
-const displayAttack = () => {};
+const handleCellClick = (row, col, playerOrComputer, cell) => {
+  playerOrComputer.board.recieveAttack(row, col);
+
+  if (playerOrComputer.board.board[row][col] == null) {
+    cell.style.backgroundColor = "blue";
+  } else if (playerOrComputer.board.board[row][col] instanceof Ship) {
+    cell.style.backgroundColor = "red";
+  }
+};
 
 computer.populateBoard();
 displayComputer();
