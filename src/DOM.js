65175453a61c7bee5cd3495526ffc0307452ee94
Nicholas-Battista <battistan23@gmail.com>
2024-03-06 18:00:24 -0700

    created mutliple funcs for placing ships
    displayPlaceGrid creates the grid to place ships
    placeShips goes through the shipLength array and
    tries to place ships. toggle direction, toggles
    ship direction

--- src/DOM.js
+++ src/DOM.js
@@ -3,10 +3,53 @@ import Ship from "./ship";
 
 const player = new Player();
 const computer = new Computer();
-const divArray = [];
+let divArray = [];
+let shipCounter = 0;
+let isHorizontal = true;
+
+const generatePlaceShipGrid = () => {
+  const placeBoard = document.querySelector(".place-board");
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const cell = document.createElement("div");
+      placeBoard.appendChild(cell);
+
+      cell.addEventListener("click", () => placeShips(i, j));
+    }
+  }
+};
+
+const placeShips = (row, col) => {
+  const shipLengths = [5, 4, 3, 3, 2];
+
+  if (
+    player.board.placeShip(shipLengths[shipCounter], row, col, isHorizontal) !==
+    null
+  ) {
+    player.board.placeShip(shipLengths[shipCounter], row, col, isHorizontal);
+    displayPlayer();
+    shipCounter++;
+  }
+};
+
+document
+  .querySelector(".direction")
+  .addEventListener("click", () => handleDirectionToggle());
+
+const handleDirectionToggle = () => {
+  if (isHorizontal) {
+    document.querySelector(".direction").textContent = "Vertical";
+    isHorizontal = false;
+  } else if (!isHorizontal) {
+    document.querySelector(".direction").textContent = "Horizontal";
+    isHorizontal = true;
+  }
+};
 
 const displayPlayer = () => {
   const playerBoard = document.querySelector(".playerBoard");
+  playerBoard.innerHTML = "";
+  divArray = [];
 
   for (let i = 0; i < 10; i++) {
     const subArray = [];
@@ -72,13 +115,14 @@ const determineAllSunk = () => {
   }
 };
 
+generatePlaceShipGrid();
 computer.populateBoard();
 displayComputer();
-player.board.placeShip(5, 0, 0, false);
-player.board.placeShip(4, 0, 1, false);
-player.board.placeShip(3, 0, 2, false);
-player.board.placeShip(3, 0, 3, false);
-player.board.placeShip(2, 0, 4, false);
+// player.board.placeShip(5, 0, 0, false);
+// player.board.placeShip(4, 0, 1, false);
+// player.board.placeShip(3, 0, 2, false);
+// player.board.placeShip(3, 0, 3, false);
+// player.board.placeShip(2, 0, 4, false);
 displayPlayer();
 
 export { displayPlayer };
